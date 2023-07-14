6a0ec58076977c7629d42a5c5c40a4dcdf6a5c7c
KacperNarwojsz <kacper.narwojsz@gmail.com>
2023-07-14 17:37:56 +0200

    almost there

--- src/pages/Cthulhu.js
+++ src/pages/Cthulhu.js
@@ -1,18 +1,20 @@
 import React from "react";
-// import EncountersCards from "../Components/EncountersCards";
+import EncountersCards from "../Components/EncountersCards";
 import eldrich_board from "../pictures/Eldrich_board.png";
-// import CthulhuBoard from "../Components/CthulhuBoard";
+import CthulhuBoard from "../Components/CthulhuBoard";
+// import EldrichBox from "../pictures/EldrichBox.jpg"
 import './AllPages.css'
 
 
 const Cthulhu = ({ submitAnswerToMain }) => {
     return (
         <div className='Cthulhu'>
-                <img src={eldrich_board} className="eldrich_board" alt="Eldrich_board" />
-            {/* <div className="CthulhuCards">
-                <CthulhuBoard />
-                <EncountersCards />
-            </div> */}
+            <div className="CthulhuContainer">
+                {/* <div className="test"><img src={EldrichBox} alt={EldrichBox} /></div> */}
+                <div className="EldrichBoardDiv"><img src={eldrich_board} className="eldrich_board" alt="Eldrich_board" /></div>
+                <div className="CthuluBoardDiv"><CthulhuBoard /></div>
+                <div className="EncountersCardsDiv"><EncountersCards /></div>
+            </div>
         </div>
     )
 }
