bbb154839d4afd14ce7b92426b9674ddc1351bd9
KacperNarwojsz <kacper.narwojsz@gmail.com>
2023-07-18 14:41:38 +0200

    The size of the board might fit

---
+++ src/Components/EldritchBoard.js
@@ -0,0 +1,15 @@
+import React from "react";
+import eldritch_board from "../pictures/Eldritch_board.png";
+import './EldritchBoard.css'
+
+
+const EldritchBoard = ({ children }) => {
+    return (
+        <div className='EldritchBoard'>
+            <img src={eldritch_board} className="eldritch_board" alt="eldritch_board" />
+            {children}
+        </div>
+    )
+}
+
+export default EldritchBoard;
--- src/pages/Cthulhu.js
+++ src/pages/Cthulhu.js
@@ -1,20 +1,18 @@
 import React from "react";
 import EncountersCards from "../Components/EncountersCards";
-import eldritch_board from "../pictures/Eldritch_board.png";
+// import eldritch_board from "../pictures/Eldritch_board.png";
 import CthulhuBoard from "../Components/CthulhuBoard";
-// import EldrichBox from "../pictures/EldrichBox.jpg"
+import EldritchBoard from "../Components/EldritchBoard";
 import './AllPages.css'
 
 
 const Cthulhu = ({ submitAnswerToMain }) => {
     return (
-        <div className='Cthulhu'>
-            <div className="CthulhuContainer">
-                {/* <div className="test"><img src={EldrichBox} alt={EldrichBox} /></div> */}
-                <div className="EldritchBoardDiv"><img src={eldritch_board} className="eldritch_board" alt="Eldritch_board" /></div>
-                <div className="CthuluBoardDiv"><CthulhuBoard /></div>
-                <div className="EncountersCardsDiv"><EncountersCards /></div>
-            </div>
+        <div className="Cthulhu">
+            <EldritchBoard>
+                {/* <CthulhuBoard />
+                <EncountersCards /> */}
+            </EldritchBoard>
         </div>
     )
 }
