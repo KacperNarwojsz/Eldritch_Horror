b644ee8a6e92f64439e0d1a4b745916f5fdd6712
KacperNarwojsz <kacper.narwojsz@gmail.com>
2023-07-20 16:07:13 +0200

    first template

--- src/Components/CthulhuBoard.js
+++ src/Components/CthulhuBoard.js
@@ -3,10 +3,16 @@ import './Components.css'
 
 const CthulhuBoard = ({ submitAnswerToHome }) => {
     return (
-        <div className='CthulhuBoard'>
+        <div>
             <button className='CthulhuSheet'></button> 
             <button className='CthulhuMystery'></button>
+            <button className='CthulhuMysteryFront'></button>
             <button className='Mythos'></button>
+            <button className='Mythos1'></button>
+            <button className='Mythos2'></button>
+            <button className='Mythos3'></button>
+            <button className='Mythos4'></button>
+            <button className='Mythos5'></button>
         </div>
     )
 }
