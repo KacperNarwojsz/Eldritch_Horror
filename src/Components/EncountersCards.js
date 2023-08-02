e2b1fe0f887428279c7a7ed82c673294bee79b23
KacperNarwojsz <kacper.narwojsz@gmail.com>
2023-08-02 15:08:57 +0200

    first progress on popup

--- src/Components/EncountersCards.js
+++ src/Components/EncountersCards.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Popup from 'reactjs-popup';
 import './Components.css'
 
 
@@ -7,6 +8,7 @@ class EncountersCards extends Component {
         super(props); 
         this.state = {
             isLoadDone: false,
+            americaDraw: 0,
         }
     }
 
@@ -20,10 +22,22 @@ class EncountersCards extends Component {
         }, 6000)
     }
 
-    render() {
+    setAmericaDraw = (event) => {
+        this.setState({ americaDraw: 2})
+      }
+
+    //   className={`encounterAmericaFront${americaDraw}`}
+
+    // <button className='encounterCardFront'></button><br></br>
+
+
+    render(americaDraw, setAmericaDraw, togglePopup, isOpen) {
         return (
         <div className='encounters'>
-            <button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='America'></button>
+            <Popup contentStyle={{width: "15.9vw", height: "25vw", padding: 0, borderRadius: "0.4vw"}} trigger=
+                {<button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='America'></button>}modal nested>
+                {close => (<div className='encounterCardFront' id='encounterAmericaFront1'><button className='encounterCloseButton' onClick={() => close()}>X</button></div>)}
+            </Popup>
             <button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='Europe'></button>
             <button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='AsiaAustralia'></button>
             <button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='General'></button>
