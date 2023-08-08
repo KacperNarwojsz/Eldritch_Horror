78d2d22ff42687759540849ee63ef24c988ba9ba
KacperNarwojsz <kacper.narwojsz@gmail.com>
2023-08-08 15:47:00 +0200

    added cards all cards except Expedition

--- src/Components/EncountersCards.js
+++ src/Components/EncountersCards.js
@@ -7,7 +7,7 @@ class EncountersCards extends Component {
     constructor(props) {
         super(props); 
         this.shuffleDeckAmerica = [1,2,3,4,5,6,7,8]
-        this.shuffleDeckEuropa = [1,2,3,4,5,6,7,8]
+        this.shuffleDeckEurope = [1,2,3,4,5,6,7,8]
         this.shuffleDeckAsiaAustralia = [1,2,3,4,5,6,7,8]
         this.shuffleDeckGeneral = [1,2,3,4,5,6,7,8,9,10,11,12]
         this.shuffleDeckOtherWorld = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
@@ -18,6 +18,13 @@ class EncountersCards extends Component {
         this.state = {
             isLoadDone: false,
             americaShuffle: 0,
+            europeShuffle: 0,
+            asiaamericaShuffle: 0,
+            generalShuffle: 0,
+            otherworldShuffle: 0,
+            expeditionShuffle: 0,
+            cthulhuresearchShuffle: 0,
+            cthulhuspecialShuffle: 0,
         }
     }
 
@@ -38,50 +45,50 @@ class EncountersCards extends Component {
         }
     }
 
-    handleEuropaShuffle = () => {
-        this.setState({americaShuffle: chooseRandom(this.shuffleDeckEuropa)});
-        if (this.shuffleDeckEuropa.length < 1) {
-            this.shuffleDeckEuropa = [1,2,3,4,5,6,7,8]
+    handleEuropeShuffle = () => {
+        this.setState({europeShuffle: chooseRandom(this.shuffleDeckEurope)});
+        if (this.shuffleDeckEurope.length < 1) {
+            this.shuffleDeckEurope = [1,2,3,4,5,6,7,8]
         }
     }
 
     handleAsiaAustraliaShuffle = () => {
-        this.setState({americaShuffle: chooseRandom(this.shuffleDeckAsiaAustralia)});
+        this.setState({asiaamericaShuffle: chooseRandom(this.shuffleDeckAsiaAustralia)});
         if (this.shuffleDeckAsiaAustralia.length < 1) {
             this.shuffleDeckAsiaAustralia = [1,2,3,4,5,6,7,8]
         }
     }
 
     handleGeneralShuffle = () => {
-        this.setState({americaShuffle: chooseRandom(this.shuffleDeckGeneral)});
+        this.setState({generalShuffle: chooseRandom(this.shuffleDeckGeneral)});
         if (this.shuffleDeckGeneral.length < 1) {
             this.shuffleDeckGeneral = [1,2,3,4,5,6,7,8,9,10,11,12]
         }
     }
 
     handleOtherWorldShuffle = () => {
-        this.setState({americaShuffle: chooseRandom(this.shuffleDeckOtherWorld)});
+        this.setState({otherworldShuffle: chooseRandom(this.shuffleDeckOtherWorld)});
         if (this.shuffleDeckOtherWorld.length < 1) {
             this.shuffleDeckOtherWorld = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
         }
     }
 
     // handleExpeditionShuffle = () => {
-    //     this.setState({americaShuffle: chooseRandom(this.shuffleDeckExpedition)});
+    //     this.setState({expeditionShuffle: chooseRandom(this.shuffleDeckExpedition)});
     //     if (this.shuffleDeckExpedition.length < 1) {
     //         this.shuffleDeckExpedition = [...]
     //     }
     // }
 
     handleCthulhuResearchShuffle = () => {
-        this.setState({americaShuffle: chooseRandom(this.shuffleDeckCthulhuResearch)});
+        this.setState({cthulhuresearchShuffle: chooseRandom(this.shuffleDeckCthulhuResearch)});
         if (this.shuffleDeckCthulhuResearch.length < 1) {
             this.shuffleDeckCthulhuResearch = [1,2,3,4,5,6,7,8]
         }
     }
 
     handleCthulhuSpecialShuffle = () => {
-        this.setState({americaShuffle: chooseRandom(this.shuffleDeckCthulhuSpecial)});
+        this.setState({cthulhuspecialShuffle: chooseRandom(this.shuffleDeckCthulhuSpecial)});
         if (this.shuffleDeckCthulhuSpecial.length < 1) {
             this.shuffleDeckCthulhuSpecial = [1,2,3,4,5,6]
         }
@@ -90,17 +97,45 @@ class EncountersCards extends Component {
     render() {
         return (
         <div className='encounters'>
+            {/* <p>{`America no. ${this.state.americaShuffle} array ${this.shuffleDeckAmerica}`}</p>
+            <p>{`Europe no. ${this.state.europeShuffle} array ${this.shuffleDeckEurope}`}</p>
+            <p>{`AsiaAustralia no. ${this.state.asiaamericaShuffle} array ${this.shuffleDeckAsiaAustralia}`}</p>
+            <p>{`General no. ${this.state.generalShuffle} array ${this.shuffleDeckGeneral}`}</p>
+            <p>{`OtherWorld no. ${this.state.otherworldShuffle} array ${this.shuffleDeckOtherWorld}`}</p>
+            <p>{`CthulhuResearch no. ${this.state.cthulhuresearchShuffle} array ${this.shuffleDeckCthulhuResearch}`}</p>
+            <p>{`CthulhuSpecial no. ${this.state.cthulhuspecialShuffle} array ${this.shuffleDeckCthulhuSpecial}`}</p> */}
             <Popup onOpen={this.handleAmericaShuffle} contentStyle={{width: "15.9vw", height: "25vw", background:'transparent', border: 'transparent'}} trigger=
                 {<button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='America'></button>}modal nested>
                 {close => (<div className='outerPopup'><div className='encounterCardFront' id={`AmericaFront${this.state.americaShuffle}`}><button className='encounterCloseButton' onClick={() => close()}>X</button></div></div>)}
             </Popup>
-            <button onClick={this.handleAmericaShuffle} className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='Europe'></button>
-            <button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='AsiaAustralia'></button>
-            <button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='General'></button>
-            <button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='OtherWorld'></button>
-            <button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='Expedition'></button>
-            <button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='Ancient_research'></button>
-            <button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='Ancient_special'></button>
+            <Popup onOpen={this.handleEuropeShuffle} contentStyle={{width: "15.9vw", height: "25vw", background:'transparent', border: 'transparent'}} trigger=
+                {<button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='Europe'></button>}modal nested>
+                {close => (<div className='outerPopup'><div className='encounterCardFront' id={`EuropeFront${this.state.europeShuffle}`}><button className='encounterCloseButton' onClick={() => close()}>X</button></div></div>)}
+            </Popup>
+            <Popup onOpen={this.handleAsiaAustraliaShuffle} contentStyle={{width: "15.9vw", height: "25vw", background:'transparent', border: 'transparent'}} trigger=
+                {<button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='AsiaAustralia'></button>}modal nested>
+                {close => (<div className='outerPopup'><div className='encounterCardFront' id={`AsiaAustraliaFront${this.state.asiaamericaShuffle}`}><button className='encounterCloseButton' onClick={() => close()}>X</button></div></div>)}
+            </Popup>
+            <Popup onOpen={this.handleGeneralShuffle} contentStyle={{width: "15.9vw", height: "25vw", background:'transparent', border: 'transparent'}} trigger=
+                {<button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='General'></button>}modal nested>
+                {close => (<div className='outerPopup'><div className='encounterCardFront' id={`GeneralFront${this.state.generalShuffle}`}><button className='encounterCloseButton' onClick={() => close()}>X</button></div></div>)}
+            </Popup>
+            <Popup onOpen={this.handleOtherWorldShuffle} contentStyle={{width: "15.9vw", height: "25vw", background:'transparent', border: 'transparent'}} trigger=
+                {<button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='OtherWorld'></button>}modal nested>
+                {close => (<div className='outerPopup'><div className='encounterCardFront' id={`OtherWorldFront${this.state.otherworldShuffle}`}><button className='encounterCloseButton' onClick={() => close()}>X</button></div></div>)}
+            </Popup>
+            <Popup onOpen={this.handleExpeditionShuffle} contentStyle={{width: "15.9vw", height: "25vw", background:'transparent', border: 'transparent'}} trigger=
+                {<button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='Expedition'></button>}modal nested>
+                {close => (<div className='outerPopup'><div className='encounterCardFront' id={`ExpeditionFront${this.state.expeditionShuffle}`}><button className='encounterCloseButton' onClick={() => close()}>X</button></div></div>)}
+            </Popup>
+            <Popup onOpen={this.handleCthulhuResearchShuffle} contentStyle={{width: "15.9vw", height: "25vw", background:'transparent', border: 'transparent'}} trigger=
+                {<button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='CthulhuResearch'></button>}modal nested>
+                {close => (<div className='outerPopup'><div className='encounterCardFront' id={`CthulhuResearchFront${this.state.cthulhuresearchShuffle}`}><button className='encounterCloseButton' onClick={() => close()}>X</button></div></div>)}
+            </Popup>
+            <Popup onOpen={this.handleCthulhuSpecialShuffle} contentStyle={{width: "15.9vw", height: "25vw", background:'transparent', border: 'transparent'}} trigger=
+                {<button className={this.state.isLoadDone ? 'encounterCard' : 'encounterCardStamp'} id='CthulhuSpecial'></button>}modal nested>
+                {close => (<div className='outerPopup'><div className='encounterCardFront' id={`CthulhuSpecialFront${this.state.cthulhuspecialShuffle}`}><button className='encounterCloseButton' onClick={() => close()}>X</button></div></div>)}
+            </Popup>            
         </div>
         )
     }
