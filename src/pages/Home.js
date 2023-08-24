50785b0b07b765ba83acb98693e12cee6a3ea0cd
KacperNarwojsz <kacper.narwojsz@gmail.com>
2023-08-24 11:41:26 +0200

    Loading page check

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
-import eldritch_logo from "../pictures/Main/Eldritch_logo.png";
+// import eldritch_logo from "../pictures/Main/Eldritch_logo.png";
+import CthulhuGif from "../pictures/Main/CthuluGif.gif"
 import './AllPages.css'
 
 
@@ -8,7 +9,8 @@ const Home = ({ loadAzathoth, loadYog, loadShub, loadCthulhuLvlChar }) => {
     return (
         <div className='home'>
             <div>
-                <img src={eldritch_logo} className="main_eldritch_logo" alt="eldritch_logo" />
+                {/* <img src={eldritch_logo} className="main_eldritch_logo" alt="eldritch_logo" /> */}
+                <img src={CthulhuGif} className="main_eldritch_logo" alt="eldritch_logo" />
             </div>
             <div>
                 <h1 className="mainTitle">WYBIERZ PRZEDWIECZNEGO:</h1>
